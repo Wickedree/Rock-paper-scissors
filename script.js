a9932e6b8c8228204286769c0fdf4e3471d9279a
Ariel <259137096+Wickedree@users.noreply.github.com>
2026-09-17 09:32:11 +0100

    Writer User Choice function

--- script.js
+++ script.js
@@ -2,16 +2,34 @@
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3) + 1;
 
-    let play;
+    let computerPlay;
 
     if (randomNum === 1) {
-        play = "rock";
+        computerPlay = "rock";
     } else if (randomNum === 2) {
-        play = "paper";
+        computerPlay = "paper";
     } else {
-        play = "scissors";
+        computerPlay = "scissors";
     }
-    console.log(play);
+    console.log(computerPlay);
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Please enter your play (use only lowercase)");
+    
+    let userPlay
+
+    if (userInput === "rock") {
+        userPlay = "rock";
+    } else if (userInput ==="paper") {
+        userPlay = "paper";
+    } else if (userInput === "scissors") {
+        userPlay = "scissors"
+    } else {
+        userPlay = "No Play";
+    }
+    console.log(userPlay);
 }
 
 getComputerChoice();
+getHumanChoice();
